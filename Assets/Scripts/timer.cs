919a250fb28c9aa0357e12d669f5b6889cce67aa
Mike Zielewski <mziele1@lsu.edu>
2017-01-21 13:08:36 -0600

    Fix timer substring length error

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -19,7 +19,19 @@ public class timer : MonoBehaviour {
 	void Update () {
         //timerCount += Time.deltaTime;
         timerCount = timerCount.Add(TimeSpan.FromSeconds(Time.deltaTime));
-        string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}", timerCount.Minutes, timerCount.Seconds, timerCount.Milliseconds.ToString().Substring(0,2));
+        string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}", timerCount.Minutes, timerCount.Seconds, returnShortMS(timerCount.Milliseconds));
         timerText.text = timeText;
+    }
+
+    string returnShortMS (int milliseconds)
+    {
+        string strMilliseconds = milliseconds.ToString();
+        if (strMilliseconds.Length > 2)
+            strMilliseconds = strMilliseconds.Substring(0, 2);
+        else if (strMilliseconds.Length == 1)
+            strMilliseconds = strMilliseconds.Substring(0, 1) + "0";
+        else if (strMilliseconds.Length == 0)
+            strMilliseconds = "00";
+        return strMilliseconds;
     }
 }
