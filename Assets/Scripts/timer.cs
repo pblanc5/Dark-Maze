0bc0a3aea2fe2ae6e4dc40a82393180132ea0083
Mike Zielewski <mziele1@lsu.edu>
2017-01-21 09:41:49 -0600

    Add timer

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    float timerCount = 0.0F;
+    public Text timerText;
+	// Update is called once per frame
+	void Update () {
+        timerCount += Time.deltaTime;
+        timerText.text = "Time: " + Mathf.Round(timerCount);
+	}
+}
