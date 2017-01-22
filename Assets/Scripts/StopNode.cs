df3f054b869afa34f1f1b2d9efd8e78111552190
Mike Zielewski <mziele1@lsu.edu>
2017-01-22 14:55:46 -0600

    Fix scoreboard

--- Assets/Scripts/StopNode.cs
+++ Assets/Scripts/StopNode.cs
@@ -2,25 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class StopNode : MonoBehaviour {
 
     private GameObject stopNode;
     private GameObject player;
+    private GameObject gm;
+    private TimeSpan timer;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        gm = GameObject.Find("GameManager");
         stopNode = this.gameObject;
-        player = GameObject.FindGameObjectWithTag("player");
+        player = GameObject.FindGameObjectWithTag("Player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
 
     public void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(0);
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            writeScore();
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    void writeScore()
+    {
+        timer = gm.GetComponent<timer>().getTimerCount();
+        Debug.Log("hello this is time" + timer.ToString());
+        Database DB = getDB();
+        DB.writeEntry("testName", timer);
+    }
+
+    Database getDB()
+    {
+        Database DB = gameObject.AddComponent<Database>();
+        DB.initDB();
+        return DB;
     }
 }
