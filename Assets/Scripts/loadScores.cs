78584160b42e743412a628c037b136f818bb1067
Mike Zielewski <mziele1@lsu.edu>
2017-01-21 20:48:46 -0600

    add scoreboard scene

---
+++ Assets/Scripts/loadScores.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadScores : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i <= 8, i++)
+        {
+            loadScore(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void loadScore(int position)
+    {
+
+    }
+}
