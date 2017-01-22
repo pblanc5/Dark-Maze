3606a85ca28c800e0ffdd1a4f30d60cdbfdb8679
Thomas Culotta <thomasculotta94@gmail.com>
2017-01-22 00:03:05 -0600

    timer stop func

--- Assets/Scripts/timer.cs
+++ Assets/Scripts/timer.cs
@@ -6,21 +6,33 @@ using System;
 
 public class timer : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 
     //float timerCount = 0.0F;
     public Text timerText;
     TimeSpan timerCount;
+
+    private bool finished;
     
+	// Use this for initialization
+	void Start ()
+    {
+        finished = false;
+	}
+
 	// Update is called once per frame
 	void Update () {
         //timerCount += Time.deltaTime;
-        timerCount = timerCount.Add(TimeSpan.FromSeconds(Time.deltaTime));
+        if (!finished)
+            timerCount = timerCount.Add(TimeSpan.FromSeconds(Time.deltaTime));
+        else
+            timerText.color = new Color(100, 200, 100);
         string timeText = string.Format("{0:D2}:{1:D2}:{2:D2}", timerCount.Minutes, timerCount.Seconds, returnShortMS(timerCount.Milliseconds));
         timerText.text = timeText;
+    }
+
+    public void StopTimer()
+    {
+        finished = true;
     }
 
     string returnShortMS (int milliseconds)
